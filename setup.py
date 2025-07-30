345e632f700134f3a569006e47de2b78e405ec61
North <northstonks@gmail.com>
2025-07-29 22:51:34 -0500

    Primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miQuintoRepo",                            # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Gabriel Carpio",                        # Tu nombre
+    author_email="carpioochoagabriel@gmail.com",    # Tu correo electrónico
+    url="https://github.com/GCO-01/miQuintoRepo",    # URL del proyecto
+)
